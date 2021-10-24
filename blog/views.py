277b12030096a1559b632fbd687976ab750ac891
mohamedfazilk <mohamedfazil961@gmail.com>
2021-10-24 17:12:26 +0530

    BlogCreateView added and post_new urls added

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 
 class BlogListView(ListView):
     model = Post
@@ -10,5 +11,11 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name ='blog/post_detail.html'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'blog/post_new.html'
+    fields = ['title', 'author', 'body']
+
 
 
