0d9305b708cdce3ecb1594fefa24749de2ecebb7
mohamedfazilk <mohamedfazil961@gmail.com>
2021-10-24 14:59:18 +0530

    post changed to object in post_detail.html

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 class BlogListView(ListView):
     model = Post
     template_name = 'blog/home.html'
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name ='blog/post_detail.html'
 
 
